646b7dcb604a8512fd5b65ba8f486b5b11fef0ef
Michael Knudsen <44273976+michaelknudsen@users.noreply.github.com>
2019-11-05 13:30:32 -0500

    Create solution.java

---
+++ java/Are the numbers in order/solution.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class main {
+	public static void print(Object obby) {
+		System.out.println(obby);
+	}
+	public static void main(String[] args) {
+		boolean a = isAscOrder(new int[] {1, 4, 13, 97, 508, 1047, 20058});
+		print(a);//true
+		boolean b = isAscOrder(new int[] {56, 98, 123, 67, 742, 1024, 32, 90969});
+		print(b);//false
+	}
+	public static boolean isAscOrder(int[] arr) {
+		int[] temp = arr.clone(); 
+		//create a clone array the use sort to put it into ascending  order.
+		Arrays.sort(temp); 
+		if(Arrays.equals(temp, arr)) {
+			return true;
+		}else {
+			return false;
+		}
+	  }
+}
